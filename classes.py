c28412d409bd59b996d5e4e5174f291db67ec4b3
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2022-12-28 12:55:28 -0800

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,14 @@
+class User:
+    name = ''
+    email = ''
+    password = '38293'
+    student_id = 0
+
+class student(User):
+    base_gpa = 3.5
+    grade = '11th Grade'
+
+class student_info(User):
+    address = '4848 leaf st'
+    phone = '503-498-4827'
+    
