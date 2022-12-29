6cf7e9c17bd01dad0607fa4c1ba809360faf350e
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2022-12-29 10:44:34 -0800

    Fixed database

---
+++ class.py
@@ -0,0 +1,11 @@
+
+class Name:
+    variable1 = 'Hello'
+    variable2 = 'hey'
+
+
+
+
+if __name__ == "__main__":
+    x = Name()
+    print(x.variable1)
--- database.py
+++ database.py
@@ -1,6 +1,10 @@
 import sqlite3
 
+
+
 conn = sqlite3.connect('test1.db')
+
+fileList = ('information.docx', 'hello.txt', 'myimage.png', 'mymovie.mpg', 'world.txt', 'data.pdf', 'myphoto.jpg')
 
 with conn:
     cur  = conn.cursor()
@@ -14,29 +18,19 @@ conn = sqlite3.connect('test1.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_persons(col_fname) VALUES (?)", \
-                ('information.docx'))
-    cur.execute("INSERT INTO tbl_persons(col_fname) VALUES (?)", \
-                ('hello.txt'))
-    cur.execute("INSERT INTO tbl_persons(col_fname) VALUES (?)", \
-                ('myimage.png'))
-    cur.execute("INSERT INTO tbl_persons(col_fname) VALUES (?)", \
-                ('mymovie.mpg'))
-    cur.execute("INSERT INTO tbl_persons(col_fname) VALUES (?)", \
-                ('world.txt'))
-    cur.execute("INSERT INTO tbl_persons(col_fname) VALUES (?)", \
-                ('data.pdf'))
-    cur.execute("INSERT INTO tbl_persons(col_fname) VALUES (?)", \
-                ('myphoto.jpg'))
+    for item in fileList:
+        if item.endswith('.txt'):
+            cur.execute('INSERT INTO tbl_files(col_fname) VALUES (?)', (item,))
     conn.commit()
 conn.close()
 
-fileList = ('information.docx', 'hello.txt', 'myimage.png', 'mymovie.mpg', 'world.txt', 'data.pdf', 'myphoto.jpg')
 
-conn = sqlite3.connect('test.db')
+
+conn = sqlite3.connect('test1.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("SELECT col_fname FROM tbl_files WHERE col_fname = '.[txt]'")
+    cur.execute("SELECT col_fname FROM tbl_files")
     varFile = cur.fetchall()
     print(varFile)
+conn.close()
