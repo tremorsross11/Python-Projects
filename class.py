3e4432a4d1b5dcd36dbd3c4f98fa96d3006966b6
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2022-12-29 11:06:26 -0800

    Created a parent class and child class

--- class.py
+++ class.py
@@ -1,11 +1,49 @@
+#parent class
+class Organism:
+    name = 'Hello'
+    species = 'hey'
+    legs = 0
+    arms = 0
+    dna = 'sequence a'
+    origin = 'unknown'
+    carbon_based = True
 
-class Name:
-    variable1 = 'Hello'
-    variable2 = 'hey'
+    def information(self):
+        msg = "\nName: {}\nSpecies: {}\nLegs: {}\nArms: {}\nDNA: {}\nOrigin: {}\nCarbon based: {}".format(self.name, self.species, self.legs, self.arms, self.dna, self.origin, self.carbon_based)
+        return msg
+#Child class
+class Human(Organism):
+    name = 'Ross'
+    species = 'human'
+    legs = 2
+    arms = 2
+    origin = 'earth'
+
+    def ingenuity(self):
+        msg = "\nCreate a deadly weapon"
+        return msg
+
+class Dog(Organism):
+    name = "Spot"
+    species = 'dog'
+    legs = 4
+    arms = 0
+    dna = 'sequence b'
+    origin = 'earth'
+
+    def bit(self):
+        msg = "\nbarks a lot"
+        return msg
+
 
 
 
 
 if __name__ == "__main__":
-    x = Name()
-    print(x.variable1)
+    x = Human()
+    print(x.information())
+    print(x.ingenuity())
+
+    dog = Dog()
+    print(dog.information())
+    print(dog.bit())
--- classes.py
+++ classes.py
@@ -4,11 +4,34 @@ class User:
     password = '38293'
     student_id = 0
 
+    def getlogininfo(self):
+        student_name = input("Enter your name: ")
+        student_email = input("Enter your email: ")
+        student_password = input("Enter your password: ")
+        if (student_email == self.email and student_password == self.password):
+            print("Welcome back, {}!".format(student_name))
+        else:
+            print("Wrong email or password")
+
 class student(User):
     base_gpa = 3.5
     grade = '11th Grade'
+
+    def getlogininfo(self):
+        student_name = input("Enter your name: ")
+        student_email = input("Enter your email: ")
+        student_pin = input("Enter your pin: ")
+        if (student_email == self.email and student_pin == self.pin):
+            print("Welcome back, {}!".format(student_name))
+        else:
+            print("Wrong email or pin")
 
 class student_info(User):
     address = '4848 leaf st'
     phone = '503-498-4827'
     
+schoolwebsite = User()
+schoolwebsite.getlogininfo()
+
+onlineclass = student()
+onlineclass.getlogininfo()
