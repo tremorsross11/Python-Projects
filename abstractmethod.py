ad12e17f3b04f63259feb67687cfcae21183e505
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-01-02 14:08:10 -0800

    Update abstractmethod.py

--- abstractmethod.py
+++ abstractmethod.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class Height(ABC):
     def tall(self, inches):
-        print("You are {} inches tall!".format(inches))
+        print("Lets see how much you've grown!")
 
     @abstractmethod
     def oldtall(self, inches):
@@ -10,9 +10,11 @@ class Height(ABC):
 
 class diagnosis(Height):
     def oldtall(self, inches):
+        tall = input("Input height: ")
+        oldtall = "5"
         print("You are {} inches tall. You grew {} inches this year!".format(tall, oldtall))
 
 
 obj = diagnosis()
-obj.tall("72")
+obj.tall(input)
 obj.oldtall("5")
